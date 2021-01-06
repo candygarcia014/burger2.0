d4967ac8a0ca83958e6ed07a8c8b87d80ce99d82
Candy Garcia <candygarcia014@gmail.com>
2021-01-06 08:52:42 -0800

    set up jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 require("dotenv").config()
     // Set up MySQL connection.
 var mysql = require("mysql");
+var connection
+if (process.env.JAWSDB_URL) {
+    connection = process.env.JAWSDB_URL
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.LOCAL_MYSQL_PW,
+        database: "burgers_db"
+    });
+}
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.LOCAL_MYSQL_PW,
-    database: "burgers_db"
-});
 
 // Make connection.
 connection.connect(function(err) {
